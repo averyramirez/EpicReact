d7f4fa3e1cd417b466e66103e721e8e393f97c9f
Avery Ramirez <68864812+averyramirez@users.noreply.github.com>
2021-02-23 10:04:27 -0800

    EpicReact
    Custom component extra credit

---
+++ finishedExercises/ReactFundamentals/customComponent.js
@@ -0,0 +1,58 @@
+// Styling
+// http://localhost:3000/isolated/exercise/05.js
+
+import * as React from 'react'
+import '../box-styles.css'
+
+function Box({className = '', style, ...otherProps}) {
+  return (
+  <div 
+  className={`box ${className}`} 
+  style={{fontStyle: 'italic', ...style}}
+  {...otherProps}  
+  />
+  )
+}
+
+
+const smallBox = (
+  <Box 
+    className="box--small"
+    style={{backgroundColor: 'lightblue'}}
+    >
+    small lightblue box
+  </Box>
+)
+
+const mediumBox = (
+  <Box 
+    className="box--medium" 
+    style={{backgroundColor: 'pink'}}
+    >
+    medium pink box
+  </Box>
+)
+
+const largeBox = (
+  <Box 
+    className="box--large" 
+    style={{backgroundColor: 'orange'}}
+    >
+    large orange box
+  </Box>
+)
+
+
+
+function App() {
+  return (
+    <div>
+      {smallBox}
+      {mediumBox}
+      {largeBox}
+      <Box>sizeless box</Box>
+    </div>
+  )
+}
+
+export default App
