438546edcb7d89bc77618011a19c02b66504ad0f
Avery Ramirez <68864812+averyramirez@users.noreply.github.com>
2021-02-25 09:59:55 -0800

    EpicReact
    Using refs extra credit

---
+++ finishedExercises/ReactFundamentals/usingRefsEC.js
@@ -0,0 +1,30 @@
+// Basic Forms
+// http://localhost:3000/isolated/exercise/06.js
+
+import * as React from 'react'
+
+function UsernameForm({onSubmitUsername}) {
+  const usernameInputRef = React.useRef()
+  function handleSubmit(event) {
+    event.preventDefault()
+    const value = usernameInputRef.current.value
+    onSubmitUsername(value)
+  }
+  
+  return (
+    <form onSubmit={handleSubmit}>
+          <div>
+        <label htmlFor="usernameInput">Username:</label>
+        <input ref={usernameInputRef} id="usernameInput" type="text" />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
+
+function App() {
+  const onSubmitUsername = username => alert(`You entered: ${username}`)
+  return <UsernameForm onSubmitUsername={onSubmitUsername} />
+}
+
+export default App
