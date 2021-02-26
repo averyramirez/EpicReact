915163d61c120064c61b7756a223e26d8e2c78fb
Avery Ramirez <68864812+averyramirez@users.noreply.github.com>
2021-02-26 10:51:12 -0800

    EpicReact
    Validate lowercase extra credit

---
+++ finishedExercises/ReactFundamentals/validateLowerCaseEC.js
@@ -0,0 +1,36 @@
+// Basic Forms
+// http://localhost:3000/isolated/exercise/06.js
+
+import * as React from 'react'
+
+function UsernameForm({onSubmitUsername}) {
+  const [error, setError] = React.useState(null)
+  function handleSubmit(event) {
+    event.preventDefault()
+    onSubmitUsername(event.target.elements.usernameInput.value)
+  }
+
+  function handleChange(event) {
+    const {value} = event.target
+    const isLowerCase = value === value.toLowerCase()
+    setError(isLowerCase ? null : 'Username must be lower case')
+  }
+  
+  return (
+    <form onSubmit={handleSubmit}>
+          <div>
+        <label htmlFor="usernameInput">Username:</label>
+        <input id="usernameInput" type="text" onChange={handleChange} />
+      </div>
+      <div style={{color: 'red'}}>{error}</div>
+      <button disabled={Boolean(error)}type="submit">Submit</button>
+    </form>
+  )
+}
+
+function App() {
+  const onSubmitUsername = username => alert(`You entered: ${username}`)
+  return <UsernameForm onSubmitUsername={onSubmitUsername} />
+}
+
+export default App
