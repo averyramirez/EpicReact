6cbb47747d2bc0c76c858f815c074d5299e4a770
Avery Ramirez <68864812+averyramirez@users.noreply.github.com>
2021-03-15 10:35:42 -0700

    EpicReact
    extract state into context exercise

---
+++ finishedExercisesAdvancedPatterns/extractState.js
@@ -0,0 +1,58 @@
+// Flexible Compound Components
+// http://localhost:3000/isolated/exercise/03.js
+
+import * as React from 'react'
+import {Switch} from '../switch'
+
+// 🐨 create your ToggleContext context here
+// 📜 https://reactjs.org/docs/context.html#reactcreatecontext
+
+const ToggleContext = React.createContext()
+
+function Toggle({onToggle, children}) {
+  const [on, setOn] = React.useState(false)
+  const toggle = () => setOn(!on)
+
+  return <ToggleContext.Provider value={{on, toggle}}>
+    {children}
+  </ToggleContext.Provider>
+}
+function useToggle() {
+  return React.useContext(ToggleContext)
+}
+
+function ToggleOn({children}) {
+  const {on} = useToggle()
+  return on ? children : null
+}
+
+function ToggleOff({children}) {
+  const {on} = useToggle()
+  return on ? null : children
+}
+
+function ToggleButton(props) {
+  const {on, toggle} = useToggle()
+  return <Switch on={on} onClick={toggle} {...props} />
+}
+
+function App() {
+  return (
+    <div>
+      <Toggle>
+        <ToggleOn>The button is on</ToggleOn>
+        <ToggleOff>The button is off</ToggleOff>
+        <div>
+          <ToggleButton />
+        </div>
+      </Toggle>
+    </div>
+  )
+}
+
+export default App
+
+/*
+eslint
+  no-unused-vars: "off",
+*/
