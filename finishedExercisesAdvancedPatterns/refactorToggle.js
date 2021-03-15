53c6b26d19a2a0aaf74e34391f19614262947740
Avery Ramirez <68864812+averyramirez@users.noreply.github.com>
2021-03-15 10:11:29 -0700

    EpicReact
    refactor toggle exercise

---
+++ finishedExercisesAdvancedPatterns/refactorToggle.js
@@ -0,0 +1,40 @@
+// Compound Components
+// http://localhost:3000/isolated/exercise/02.js
+
+import * as React from 'react'
+import { findRenderedComponentWithType } from 'react-dom/test-utils'
+import {Switch} from '../switch'
+
+function Toggle({children}) {
+  const [on, setOn] = React.useState(false)
+  const toggle = () => setOn(!on)
+
+  return React.Children.map(children, child => {
+    const newChild = React.cloneElement(child, {on, toggle})
+    return newChild
+  })
+}
+
+
+const ToggleOn = ({on, children}) => on ? children : null
+const ToggleOff = ({on, children}) => on ? null : children
+const ToggleButton = ({on, toggle}) => <Switch on={on} onClick={toggle} />
+
+function App() {
+  return (
+    <div>
+      <Toggle>
+        <ToggleOn>The button is on</ToggleOn>
+        <ToggleOff>The button is off</ToggleOff>
+        <ToggleButton />
+      </Toggle>
+    </div>
+  )
+}
+
+export default App
+
+/*
+eslint
+  no-unused-vars: "off",
+*/
