3d953d5cbca7939073141f7d6781d0a864bdfe3d
Avery Ramirez <68864812+averyramirez@users.noreply.github.com>
2021-03-31 08:28:23 -0700

    Create darkThemeEC.js

---
+++ finishedExercisesTestingReactApps/darkThemeEC.js
@@ -0,0 +1,33 @@
+// testing with context and a custom render method
+// http://localhost:3000/easy-button
+
+import * as React from 'react'
+import {render, screen} from '@testing-library/react'
+import {ThemeProvider} from '../../components/theme'
+import EasyButton from '../../components/easy-button'
+
+test('renders with the light styles for the light theme', () => {
+  const Wrapper = ({children}) => {
+    <ThemeProvider intialTheme="light">{children}</ThemeProvider>
+  }
+  render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: white;
+    color: black;
+  `)
+})
+
+test('renders with the dark styles for the dark theme', () => {
+  const Wrapper = ({children}) => {
+    <ThemeProvider intialTheme="dark">{children}</ThemeProvider>
+  }
+  render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    color: white;
+    background-color: black;
+  `)
+})
+
+/* eslint no-unused-vars:0 */
